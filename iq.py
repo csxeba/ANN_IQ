b933721ca0bea4e842864806384afee04cb0848d
csa <csxeba@gmail.com>
2016-11-16 18:58:42 +0100

    Fifi commit

--- iq.py
+++ iq.py
@@ -8,30 +8,50 @@ from ANN_IQ.util import *
 
 # Define hyperparameters
 LRATE = 3.
-BSIZE = 10
-EPOCHS = 60
+BSIZE = 20
+EPOCHS = 30
+
+# Instanciate activation and cost function wrappers
+sigmoid = Sigmoid()
+mse = MSE()
+
+mnistpath = "./mnist.pkl.gz"
 
 
 class Layer:
+    """Fully connected feed forward layer (aka Dense)"""
 
     def __init__(self, inputs, neurons):
-        self.weights = np.random.randn(inputs, neurons)
-        self.biases = np.zeros(neurons)
+        """
+        Parameter initialization is done according to LeCun et al., 1998
+        We simply ignore the fact that the initialization scheme is optimized
+        for tanh layers... (we are going to use sigmoid layers)
+        """
 
+        self.weights = np.random.randn(inputs, neurons) / np.sqrt(inputs)
+        self.biases = np.zeros(neurons)
         self.activations = None
 
     def get_weights(self):
+        """Return the flattened weight matrices as 1D-vectors"""
         return self.weights.ravel()
 
     def set_weights(self, ws):
+        """Fold the ws weight vector to a matrix"""
         self.weights = ws.reshape(self.weights.shape)
 
 
 class Model:
+    """
+    Sequential (feed-forward) type of artificial neural network
+    """
 
     def __init__(self):
-        # Instanciate parameters according to LeCunn et al., 1998
-        # Weights and biases of hidden neurons
+        """
+        This network is a concrete implementation with a
+        (784-30-10) architecture to classify the MNIST
+        reference data
+        """
 
         self.hidden = Layer(784, 30)
         self.output = Layer(30, 10)
@@ -39,7 +59,8 @@ class Model:
         self.lrate = LRATE
 
     def feedforward(self, X):
-        self.inputs = X
+        """Produces output from the input 2D array (X)"""
+        self.inputs = np.copy(X)
 
         self.hidden.activations = X @ self.hidden.weights
         self.hidden.activations += self.hidden.biases
@@ -52,10 +73,12 @@ class Model:
         return self.output.activations
 
     def predict(self, X):
+        """Converts the raw predictions into actual numbers"""
         output = self.feedforward(X)
         return np.argmax(output, axis=1)
 
     def evaluate(self, X, Y):
+        """Evaluates the network's prediction accuracy"""
         preds = self.predict(X)
         targets = np.argmax(Y, axis=1)
         eq = np.equal(preds, targets)
@@ -63,13 +86,12 @@ class Model:
         return eq.sum() / len(eq)
 
     def backpropagation(self, Y, get_grads=False):
+        """Calculates the gradients and updates the parameters"""
 
-        cost = mse(Y, self.output.activations)
-
-        # backpropagate the errors
+        # calculate the output layer's error
         delta_o = mse.derivative(Y, self.output.activations) * \
                   sigmoid.derivative(self.output.activations)
-
+        # backpropagate the error to the hidden layer
         delta_h = delta_o @ self.output.weights.T * \
                   sigmoid.derivative(self.hidden.activations)
 
@@ -77,34 +99,37 @@ class Model:
         grad_Wo = self.hidden.activations.T @ delta_o
         grad_Wh = self.inputs.T @ delta_h
 
+        # if this is set to True, no parameter update is performed,
+        # the analytically calculated gradients are simply returned
         if get_grads:
             return np.concatenate([grad_Wh.ravel(), grad_Wo.ravel()])
 
-        # descend on gradients
-        m = Y.shape[0]  # batch size for gradient averaging
-        eta = self.lrate / m
+        # update the parameters according to Stochastic Gradient Descent
+        m = Y.shape[0]
+        eta = self.lrate / m  # this is for per-minibatch gradient averaging
 
+        # descend on gradients
         self.output.weights -= grad_Wo * eta
         self.output.biases -= delta_o.sum(axis=0) * eta
         self.hidden.weights -= grad_Wh * eta
         self.hidden.biases -= delta_h.sum(axis=0) * eta
 
-        return cost
-
-    def fit(self, X, Y, no_epochs, batch_size, validation):
-        N = X.shape[0]
-        assert N == Y.shape[0]
+    def fit(self, X, Y, no_epochs, batch_size, validation=()):
+        """This method coordinates the learning process"""
+        N = X.shape[0]  # nuber of total learning samples
 
         for epoch in range(1, no_epochs + 1):
             print("Epoch:", epoch)
             X, Y = shuffle_data(X, Y)
-            costs = []
-            for batch_start in range(0, N, batch_size):
-                batchX = X[batch_start:batch_start + batch_size]
-                batchY = Y[batch_start:batch_start + batch_size]
 
+            costs = []  # accumulate costs for later averaging
+            for batch_start in range(0, N, batch_size):
+                batch_end = batch_start + batch_size
+                batchX = X[batch_start:batch_end]
+                batchY = Y[batch_start:batch_end]
                 self.feedforward(batchX)
-                cost = self.backpropagation(batchY)
+                self.backpropagation(batchY)
+                cost = mse(Y, self.output.activations)
                 costs.append(cost)
 
                 print("\rCost:  {0:.5f}".format(np.mean(costs)), end="")
@@ -117,9 +142,14 @@ class Model:
                 print()
 
     def get_weights(self):
+        """
+        Get the flattened weights from the layers and return them as an
+        1D-array
+        """
         return np.concatenate([self.hidden.get_weights(), self.output.get_weights()])
 
     def set_weights(self, ws):
+        """Set layer weights. ws is an 1D-array (vector)"""
         size = self.hidden.weights.size
         self.hidden.set_weights(ws[:size])
         self.output.set_weights(ws[size:])
@@ -128,12 +158,12 @@ class Model:
         """
         Estimate gradients by the numerical differentiation formula:
 
-        f(w-epsilon) - f(w+epsilon)
-        --------------------------- ~= df/dw, if epsilon is sufficiently small.
-                 2*epsilon
+        cost(w-epsilon) - cost(w+epsilon)
+        --------------------------------- ~= df/dw, if epsilon is sufficiently small
+                    2*epsilon
 
-        If backprop was implemented correctly, the difference between the numerical
-        and analytical gradients should be minimal (~ 1e-7)
+        If backprop was implemented correctly, the relative error between
+        the numerical and analytical gradients should be minimal (~ 1e-7)
         """
 
         def get_numerical_gradients():
@@ -161,8 +191,8 @@ class Model:
 
         def get_relative_error_and_diffs():
             norm = np.linalg.norm
-            diff = analytic - numeric
-            relative_error = norm(diff) / max(norm(analytic), norm(numeric))
+            diff = analytical - numerical
+            relative_error = norm(diff) / max(norm(analytical), norm(numerical))
 
             size = self.hidden.weights.size
             hshape = self.hidden.weights.shape
@@ -195,25 +225,25 @@ class Model:
         return error, diffs
 
 
-start = time.time()
-mnistpath = "D:/Data/misc/mnist.pkl.gz"
+def main():
+    start = time.time()
 
-# Instanciate activation and cost function wrappers
-sigmoid = Sigmoid()
-mse = MSE()
+    # Read data to memory. Data is split by 60k:10k.
+    (mnistX, mnistY), (testX, testY) = pull_mnist_data(mnistpath)
 
-# Read data to memory. Data is split by 60k:10k.
-(mnistX, mnistY), (testX, testY) = pull_mnist_data(mnistpath)
+    # Instanciate the network model
+    model = Model()
+    print("INITIAL ACC:", model.evaluate(testX, testY))
 
-# Instanciate the network model
-model = Model()
-print("INITIAL ACC:", model.evaluate(testX, testY))
+    # Perform a gradient check against numerical gradients (backprop sanity check)
+    model.gradient_check(mnistX[:20], mnistY[:20])
 
-# Perform a gradient check against numerical gradients (backprop sanity check)
-model.gradient_check(mnistX[:20], mnistY[:20])
+    # Fit model to data
+    model.fit(X=mnistX, Y=mnistY, no_epochs=EPOCHS, batch_size=BSIZE,
+              validation=(testX, testY))
 
-# Fit model to data
-model.fit(X=mnistX, Y=mnistY, no_epochs=EPOCHS, batch_size=BSIZE,
-          validation=(testX, testY))
+    print("FINITE! Time required: {} s".format(int(time.time() - start)))
 
-print("FINITE! Time required: {} s".format(int(time.time() - start)))
+
+if __name__ == '__main__':
+    main()
--- reference.py
+++ reference.py
@@ -4,27 +4,31 @@ from ANN_IQ.util import pull_mnist_data
 
 
 def get_keras_net():
+    """Depends on Keras and Theano/Tensorflow"""
     from keras.models import Sequential
     from keras.layers import Dense
     from keras.optimizers import SGD
 
     model = Sequential([
         Dense(input_dim=784, output_dim=30, activation="sigmoid"),
-        Dense(input_dim=30, output_dim=10, activation="sigmoid")
+        Dense(input_dim=60, output_dim=10, activation="sigmoid")
     ])
-    model.compile(optimizer=SGD(lr=0.5), loss="mse", metrics=["acc"])
+    model.compile(optimizer=SGD(lr=3.0), loss="mse", metrics=["acc"])
 
-    return model
+    return model  # THIS IS A NETWORK OBJECT!
 
 
 def get_theano_net():
+    """Depends on theno"""
     import theano
     import theano.tensor as T
+
+    print("Defining Theano computational graph...", end=" ")
 
     inputs = T.matrix(dtype="float32")
     targets = T.matrix(dtype="float32")
 
-    eta = np.array([0.5]).astype("float32")
+    eta = np.array([3.0]).astype("float32") / inputs.shape[0].astype("float32")
 
     Wh = theano.shared(np.random.randn(784, 60).astype("float32"))
     Wo = theano.shared(np.random.randn(60, 10).astype("float32"))
@@ -34,6 +38,8 @@ def get_theano_net():
     activation = T.nnet.sigmoid(inputs.dot(Wh) + bh)
     activation = T.nnet.sigmoid(activation.dot(Wo) + bo)
 
+    prediction = T.argmax(activation, axis=1)
+
     cost = T.sum((targets - activation) ** 2)
 
     update_Wh = Wh - theano.grad(cost, wrt=Wh) * eta
@@ -41,28 +47,66 @@ def get_theano_net():
     update_bh = bh - theano.grad(cost, wrt=bh) * eta
     update_bo = bo - theano.grad(cost, wrt=bo) * eta
 
-    training = theano.function(inputs=[inputs, targets],
-                               outputs=[cost],
-                               updates=[(Wh, update_Wh),
-                                        (Wo, update_Wo),
-                                        (bh, update_bh),
-                                        (bo, update_bo)])
-    return training
+    print("Done!\nCompiling theano functions...", end=" ")
+    fit_batch = theano.function(inputs=[inputs, targets],
+                                outputs=[cost],
+                                updates=[(Wh, update_Wh),
+                                         (Wo, update_Wo),
+                                         (bh, update_bh),
+                                         (bo, update_bo)])
+
+    predict = theano.function(inputs=[inputs],
+                              outputs=[prediction])
+    print("Done!")
+    return fit_batch, predict  # THESE ARE FUNCTION POINTERS!
 
 
 def get_brainforged_net():
+    """Depends on csxnet and csxdata, both available on my github
+    (but there are no install scripts for them :) )"""
     from csxnet import Network
     from csxdata import CData, roots
 
-    model = Network(CData(roots["misc"] + "mnist.pkl.gz", 0.18), 0.5, 0, 0, 0, cost="mse")
+    model = Network(CData(roots["misc"] + "mnist.pkl.gz", 0.18),
+                    eta=3.0, lmbd1=0, lmbd2=0, mu=0, cost="mse")
     model.add_fc(60, activation="sigmoid")
-    model.finalize_architecture()
+    model.finalize_architecture(activation="sigmoid")
 
-    return model
+    return model  # THIS IS A NETWORK OBJECT!
 
-mnistpath = "/data/Prog/data/misc/mnist.pkl.gz"
 
-(X, y), validation = pull_mnist_data(mnistpath)
-network = get_keras_net()
+def train_keras():
+    print("-"*25)
+    print("Keras Training!s")
+    network = get_keras_net()
+    (X, y), validation = pull_mnist_data(mnistpath)
+    network.fit(X, y, batch_size=20, nb_epoch=30, validation_data=validation)
 
-network.fit(X, y, batch_size=20, nb_epoch=30, validation_data=validation)
+
+def train_brainforge():
+    network = get_brainforged_net()
+    network.fit(batch_size=20, epochs=30, monitor=["acc"])
+
+
+def train_theano():
+    print("-"*25)
+    print("Theano training!")
+    (X, y), validation = pull_mnist_data(mnistpath)
+
+    fit_batch, predict = get_theano_net()
+    N = X.shape[0]
+    for epoch in range(1, 30):
+        print("Epoch: {0} / {1}".format(epoch, 30))
+        for i in range(0, N, 20):
+            cost = fit_batch(X[i:i+20], y[i:i+20])[0]  # dat indexing...
+            print("\rBatch {0:>{w}} / {1}; Cost: {2:.4f}"
+                  .format(i+20, N, float(cost), w=len(str(N))), end="")
+    print()
+
+
+mnistpath = "./mnist.pkl.gz"
+
+if __name__ == '__main__':
+    # train_keras()
+    # train_theano()
+    train_brainforge()
