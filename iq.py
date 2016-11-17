dd6f9eb69515ebd87480e46d4b0fa2fe2a3e8993
Csxeba <csxeba@gmail.com>
2016-11-17 10:27:08 +0100

    Final modifications

--- iq.py
+++ iq.py
@@ -6,30 +6,42 @@ from ANN_IQ.util import *
 # Wildcard import defines cost and activation functions
 # also import numpy from the above namespace!
 
-# Define hyperparameters
+# Define hyperparameters according to the first part of
+# M. Nielsen's neural network tutorial:
+# http://neuralnetworksanddeeplearning.com/
+
 LRATE = 3.
 BSIZE = 20
 EPOCHS = 30
 
 # Instanciate activation and cost function wrappers
 sigmoid = Sigmoid()
+# tanh = Tanh()
+# relu = ReLU
 mse = MSE()
+# xent = Xent()
 
 mnistpath = "./mnist.pkl.gz"
 
 
 class Layer:
-    """Fully connected feed forward layer (aka Dense)"""
+    """
+    Fully connected feed forward layer (aka. the Dense Layer)
+    """
 
     def __init__(self, inputs, neurons):
         """
         Parameter initialization is done according to LeCun et al., 1998
         We simply ignore the fact that the initialization scheme is optimized
         for tanh layers... (we are going to use sigmoid layers)
+
+        :param: inputs: the number of input nodes
+        :param: neurons: the number of neurons
         """
 
         self.weights = np.random.randn(inputs, neurons) / np.sqrt(inputs)
         self.biases = np.zeros(neurons)
+        self.actfn = sigmoid
         self.activations = None
 
     def get_weights(self):
@@ -43,7 +55,9 @@ class Layer:
 
 class Model:
     """
-    Sequential (feed-forward) type of artificial neural network
+    Sequential (feed-forward) type of artificial neural network,
+    Architecture is the Multilayer Perceptron (MLP).
+    https://en.wikipedia.org/wiki/Multilayer_perceptron
     """
 
     def __init__(self):
@@ -58,61 +72,68 @@ class Model:
         self.inputs = None  # network input (X) goes here
         self.lrate = LRATE
 
+        self.cost = mse
+
     def feedforward(self, X):
         """Produces output from the input 2D array (X)"""
         self.inputs = np.copy(X)
 
         self.hidden.activations = X @ self.hidden.weights
         self.hidden.activations += self.hidden.biases
-        self.hidden.activations = sigmoid(self.hidden.activations)
+        self.hidden.activations = self.hidden.actfn(self.hidden.activations)
 
         self.output.activations = self.hidden.activations @ self.output.weights
         self.output.activations += self.output.biases
-        self.output.activations = sigmoid(self.output.activations)
+        self.output.activations = self.output.actfn(self.output.activations)
 
         return self.output.activations
 
-    def predict(self, X):
-        """Converts the raw predictions into actual numbers"""
-        output = self.feedforward(X)
-        return np.argmax(output, axis=1)
-
-    def evaluate(self, X, Y):
-        """Evaluates the network's prediction accuracy"""
-        preds = self.predict(X)
-        targets = np.argmax(Y, axis=1)
-        eq = np.equal(preds, targets)
-        assert len(set(ar.shape[0] for ar in (X, Y, preds, targets, eq))) == 1
-        return eq.sum() / len(eq)
-
     def backpropagation(self, Y, get_grads=False):
         """Calculates the gradients and updates the parameters"""
+        # size of the mini-batch, needed for the per-minibatch
+        # gradient averaging step
+        m = Y.shape[0]
+        eta = self.lrate / m  # can be incorporated into learning rate
 
         # calculate the output layer's error
         delta_o = mse.derivative(Y, self.output.activations) * \
-                  sigmoid.derivative(self.output.activations)
+                  self.output.actfn.derivative(self.output.activations)
         # backpropagate the error to the hidden layer
         delta_h = delta_o @ self.output.weights.T * \
-                  sigmoid.derivative(self.hidden.activations)
+                  self.output.actfn.derivative(self.hidden.activations)
 
         # calculate weight gradients
         grad_Wo = self.hidden.activations.T @ delta_o
         grad_Wh = self.inputs.T @ delta_h
 
-        # if this is set to True, no parameter update is performed,
-        # the analytically calculated gradients are simply returned
         if get_grads:
-            return np.concatenate([grad_Wh.ravel(), grad_Wo.ravel()])
+            # the parameter update is skipped and the calculated gradients
+            # are returned as a single flattened 1D-array
+            # used in self.gradient_check()
+            return np.concatenate([grad_Wh.ravel(), grad_Wo.ravel()]) / m
 
         # update the parameters according to Stochastic Gradient Descent
-        m = Y.shape[0]
-        eta = self.lrate / m  # this is for per-minibatch gradient averaging
-
-        # descend on gradients
+        # w := w - dMSE/dw; b := b - dMSE/db
         self.output.weights -= grad_Wo * eta
         self.output.biases -= delta_o.sum(axis=0) * eta
         self.hidden.weights -= grad_Wh * eta
         self.hidden.biases -= delta_h.sum(axis=0) * eta
+
+        # Gradient of the biases:
+        # -----------------------
+        # from z = X @ W + b
+        # and a = sigm(z)
+        #
+        # dMSE/db = dMSE/da * da/dz * dz/db
+        # dMSE/da = a - y
+        # da/dz is the sigmoid's derivative
+        # da/dz = sigm(z) * (1 - sigm(z)) = a * (1 - a)!
+        # dz/db = 1 (remember, its a partial derivative)
+        #
+        # So the bias' gradient = dMSE/da * da/dz * 1,
+        # which equals delta_o (summed over the batch and averaged by m)
+        # it is calculated similarily in hidden layers:
+        # delta_h, summed over the batch, averaged by m.
 
     def fit(self, X, Y, no_epochs, batch_size, validation=()):
         """This method coordinates the learning process"""
@@ -127,26 +148,46 @@ class Model:
                 batch_end = batch_start + batch_size
                 batchX = X[batch_start:batch_end]
                 batchY = Y[batch_start:batch_end]
+
                 self.feedforward(batchX)
                 self.backpropagation(batchY)
-                cost = mse(Y, self.output.activations)
+
+                cost = self.cost(batchY, self.output.activations)
                 costs.append(cost)
 
                 print("\rCost:  {0:.5f}".format(np.mean(costs)), end="")
 
             if validation:
-                print("\tLAcc: {0:.2%}\tTAcc: {0:.2%}"
-                      .format(self.evaluate(X[:10000], Y[:10000]),
-                              self.evaluate(*validation)))
+                nvalid = validation[0].shape[0]
+                learning_accuracy = self.evaluate(X[:nvalid], Y[:nvalid])
+                testing_accuracy = self.evaluate(*validation)
+                print("\tAccuracies: on learning: {0:.2%}\ton testing: {1:.2%}"
+                      .format(learning_accuracy, testing_accuracy))
             else:
                 print()
+
+    def predict(self, X):
+        """Converts the raw predictions into actual numbers"""
+        raw_predictions = self.feedforward(X)
+        return np.argmax(raw_predictions, axis=1)
+
+    def evaluate(self, X, Y):
+        """Evaluates the network's prediction accuracy"""
+        preds = self.predict(X)  # This is a vector of numbers
+        targets = np.argmax(Y, axis=1)  # so is this
+        eq = np.equal(preds, targets)  # bool vector: True, where equal, False otherwise
+        no_right_predictions = eq.sum()
+        all_predictions = len(eq)
+        # we get the rate of right predictions (the accuracy)
+        return no_right_predictions / all_predictions
 
     def get_weights(self):
         """
         Get the flattened weights from the layers and return them as an
         1D-array
         """
-        return np.concatenate([self.hidden.get_weights(), self.output.get_weights()])
+        return np.concatenate([self.hidden.get_weights(),
+                               self.output.get_weights()])
 
     def set_weights(self, ws):
         """Set layer weights. ws is an 1D-array (vector)"""
@@ -164,7 +205,10 @@ class Model:
 
         If backprop was implemented correctly, the relative error between
         the numerical and analytical gradients should be minimal (~ 1e-7)
+        Biases are not (yet?) included!
         """
+
+        # Nested functions are only used for better readability
 
         def get_numerical_gradients():
             ws = self.get_weights()
@@ -172,16 +216,16 @@ class Model:
             grads = np.copy(perturbation)
 
             rounds = str(len(ws))
-            for i in range(len(ws)):
-                print("\rCalculating numerical gradients... {0:>{w}} / {1}"
-                      .format(i+1, rounds, w=len(rounds)), end="")
+            for i in range(len(ws)):  # This is done for every weight
+                print("\rCalculating numerical gradients... {0:>{width}} / {1}"
+                      .format(i+1, rounds, width=len(rounds)), end="")
 
                 perturbation[i] += epsilon
 
                 self.set_weights(ws + perturbation)
-                cost_plus = mse(self.feedforward(X), y)
+                cost_plus = self.cost(self.feedforward(X), y)
                 self.set_weights(ws - perturbation)
-                cost_minus = mse(self.feedforward(X), y)
+                cost_minus = self.cost(self.feedforward(X), y)
 
                 grads[i] = (cost_plus - cost_minus)
                 perturbation[i] = 0.0
@@ -190,39 +234,32 @@ class Model:
             return grads / (2*epsilon)
 
         def get_relative_error_and_diffs():
-            norm = np.linalg.norm
+            norm = np.linalg.norm  # alias this function (the vector norm)
             diff = analytical - numerical
             relative_error = norm(diff) / max(norm(analytical), norm(numerical))
 
-            size = self.hidden.weights.size
-            hshape = self.hidden.weights.shape
-            oshape = self.output.weights.shape
-            hdiff = diff[:size].reshape(hshape)
-            odiff = diff[size:].reshape(oshape)
+            return relative_error
 
-            return relative_error, [hdiff, odiff]
-
-        def print_evaluation():
+        def print_evaluation(er):
             print("Gradient check:", end=" ")
-            if error < 1e-6:
+            if er < 1e-6:
                 print("PASSED!", end=" ")
-            elif error < 1e-5:
+            elif er < 1e-5:
                 print("SUSPICIOUS!", end=" ")
-            elif error < 1e-3:
+            elif er < 1e-3:
                 print("ERROR!", end=" ")
             else:
                 print("FATAL ERROR!", end=" ")
-            print("error =", error)
+            print("error =", er)
 
         self.fit(X, y, no_epochs=1, batch_size=20, validation=(X, y))
 
         numerical = get_numerical_gradients()
         analytical = self.backpropagation(y, get_grads=True)
 
-        error, diffs = get_relative_error_and_diffs()
+        error = get_relative_error_and_diffs()
 
-        print_evaluation()
-        return error, diffs
+        print_evaluation(error)
 
 
 def main():
@@ -233,7 +270,7 @@ def main():
 
     # Instanciate the network model
     model = Model()
-    print("INITIAL ACC:", model.evaluate(testX, testY))
+    print("INITIAL PREDICTION ACCURACY:", model.evaluate(testX, testY))
 
     # Perform a gradient check against numerical gradients (backprop sanity check)
     model.gradient_check(mnistX[:20], mnistY[:20])
--- util.py
+++ util.py
@@ -1,15 +1,56 @@
 import numpy as np
 
 
-def pull_mnist_data(path):
-    from csxdata.utilities.parsers import mnist_tolearningtable
-    Xs, Ys = mnist_tolearningtable(path, fold=False)
-    N = Ys.shape[0]
-    # Xs += 1e-7
+def mnist_to_learning_table(source: str):
+    """The reason of this method's existance is that I'm lazy as ..."""
+    import pickle
+    import gzip
 
+    # This hack below is needed because the supplied mnist.pkl.gz file is
+    # downloaded from Nielsen's github repo:
+    # https://github.com/mnielsen/neural-networks-and-deep-learning
+    # His file was created under Python 2 with Windows type encoding
+    # and I didn't convert it to UTF-8.
+    # Of course by the time I wrote this comment, rationalizing my
+    # laziness, I could've wrote a better, more readable implementation,
+    # yet here I am still, leaving this method as it is, instanciating
+    # private classes from of the pickle module...
+    # But alas, more disgusting things are happening all around us, so
+    # why start to change the world by changing myself first?
+    f = gzip.open(source)
+    with f:
+        u = pickle._Unpickler(f)
+        u.encoding = "latin1"
+        tup = u.load()
+    f.close()
+
+    # The original dataset is split so:
+    # - 50k learning
+    # - 10k testing (for hyperparameter optimization)
+    # - 10k validation (for final model validation)
+    # We unify them and return the complete dataset.
+    questions = np.concatenate((tup[0][0], tup[1][0], tup[2][0]))
+    questions = questions.astype("float32", copy=False)
+    targets = np.concatenate((tup[0][1], tup[1][1], tup[2][1]))
+    return questions, targets
+
+
+def pull_mnist_data(path):
+    Xs, Ys = mnist_to_learning_table(path)
+    N = Ys.shape[0]
+
+    # We create a one-hot representation from the classes is Y.
     onehot = np.zeros((N, 10), dtype="float32")
     for i, y in enumerate(Ys):
         onehot[i, y] += np.asscalar(np.array([1.0]))
+    # onehot is shaped thus:
+    # (N, 10), where N is the number of examples, 10 is the 1-hot vector of the true label:
+    # e.g. the number 6 is encoded so: [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]
+    # values of Y are stored as 32 bit floats (double)
+
+    # X is shaped thus:
+    # (N, 784), where N is the number of examples, 784 = 28x28 represents the pixels.
+    # values of X are normalized from 0.0 - 1.0 and stored as 32 bit floats (double)
 
     mnistX, mnistY = shuffle_data(Xs, onehot)
     testX, testY = mnistX[:10000], mnistY[:10000]
@@ -19,39 +60,95 @@ def pull_mnist_data(path):
 
 
 def shuffle_data(X, Y):
+    """Shuffles X and Y together"""
     shuffargs = np.arange(Y.shape[0])
     np.random.shuffle(shuffargs)
     return X[shuffargs], Y[shuffargs]
 
 
 class Sigmoid:
+    """
+    The sigmoid (logistic) activation function [0, +1],
+    mainly applied in output units, in hiddens in can be
+    applied in certain situations.
+    https://en.wikipedia.org/wiki/Sigmoid_function
+    """
     def __call__(self, z: np.ndarray) -> np.ndarray:
         return 1 / (1 + np.exp(-z))
 
     @staticmethod
     def derivative(a: np.ndarray) -> np.ndarray:
+        """Parameter <a> = sigmoid(z), see __call__(z)!"""
         return a * (1 - a)
 
     def __str__(self):
         return "Sigmoid"
 
 
+class Tanh:
+    """
+    The hyperbolic tangent activation function [-1, +1],
+    mostly applied in hidden neurons.
+    https://en.wikipedia.org/wiki/Hyperbolic_function
+    """
+    def __call__(self, Z):
+        return np.tanh(Z)
+
+    def __str__(self): return "tanh"
+
+    @staticmethod
+    def derivative(A):
+        return np.subtract(1.0, np.square(A))
+
+
 class ReLU:
+    """
+    The Rectified Linear Unit, popular in deeper architectures,
+    only used in hidden neurons.
+    https://en.wikipedia.org/wiki/Rectifier_(neural_networks)
+    """
     def __call__(self, z: np.ndarray) -> np.ndarray:
         return np.greater(z, 0) * z
 
     @staticmethod
     def derivative(a: np.ndarray) -> np.ndarray:
+        """Parameter <a> = ReLU(z), see __call__(z)!"""
         return np.greater(a, 0).astype(float)
 
 
 class MSE:
+    """
+    The Mean Squader Error cost function,
+    applicable to any type of problem (classification or regression)
+    https://en.wikipedia.org/wiki/Mean_squared_error
+    """
     def __call__(self, y: np.ndarray, a: np.ndarray) -> np.ndarray:
-        return 0.5 * np.sum((y - a)**2)
+        return 0.5 * np.sum((a - y)**2) / y.shape[0]
 
     @staticmethod
     def derivative(y: np.ndarray, a: np.ndarray) -> np.ndarray:
         return a - y
+
+    def __str__(self):
+        return "MSE"
+
+
+class Xent:
+    """
+    The cross-entropy cost function,
+    applicable only to classification problems (like MNIST)
+    https://en.wikipedia.org/wiki/Cross_entropy
+    """
+
+    def __call__(self, y: np.ndarray, a: np.ndarray) -> np.ndarray:
+        # Basically -log(a) at the "right" neuron, -log(1 - a) otherwise
+        # By right neuron I mean "the output neuron representing the correct label"
+        return -np.sum(y * np.log(a) + (1 - y) * np.log(1 - a)) / y.shape[0]
+
+    @staticmethod
+    def derivative(y: np.ndarray, a: np.ndarray) -> np.ndarray:
+        # The denominator is factored out, if used with sigmoid output units!
+        return (y - a) / ((a - 1) * a)
 
     def __str__(self):
         return "MSE"
